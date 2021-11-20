cdfe8ff39ad3a24b1027f0ff063ed29c2508fe65
dw.seo <will6678@naver.com>
2021-11-20 09:01:48 +0900

    add main.cpp for 행성터널

---
+++ 동욱/211121/행성터널/main.cpp
@@ -0,0 +1,105 @@
+// 행성 터널
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Edge {
+public:
+    Edge(int lhs, int rhs, int distance) {
+        this->node[0]  = lhs;
+        this->node[1]  = rhs;
+        this->distance = distance;
+    }
+
+    bool operator<(Edge &edge) {
+        return this->distance < edge.distance;
+    }
+
+    static int get_parent(int child);
+    static bool union_parent(int lhs, int rhs);
+    static bool is_sibling(int lhs, int rhs);
+
+public:    
+    int node[2];
+    int distance;
+
+    static vector<int> parents;    
+};
+vector<int> Edge::parents;
+
+int Edge::get_parent(int child) {
+    if(parents[child] == child)
+        return child;
+    
+    parents[child] = get_parent(parents[child]);
+
+    return parents[child];
+}
+
+bool Edge::union_parent(int lhs, int rhs) {
+    lhs = get_parent(lhs);
+    rhs = get_parent(rhs);
+
+    if(lhs < rhs)
+        parents[rhs] = lhs;
+    else
+        parents[lhs] = rhs;
+}
+
+bool Edge::is_sibling(int lhs, int rhs) {
+
+    return get_parent(lhs) == get_parent(rhs);
+}
+
+typedef struct _Pos {
+    int x, y, z;
+} Pos;
+
+int get_min_len(const Pos& lhs, const Pos& rhs) {
+    vector<int> res;
+    res.push_back(abs(lhs.x - rhs.x)); 
+    res.push_back(abs(lhs.y - rhs.y)); 
+    res.push_back(abs(lhs.z - rhs.z));
+
+    return *min_element(res.begin(), res.end());
+}
+
+int main() {
+    int N;
+    vector<Pos> loc;
+    vector<Edge> edges;
+
+    cin >> N;
+    int x, y, z;
+    for(int i=0; i<N; i++) {
+        Edge::parents.push_back(i);
+        
+        cin >> x >> y >> z;
+        loc.push_back({x, y, z});
+    }
+
+    for(int lhs=0; lhs<N; lhs++) {
+        for(int rhs=lhs+1; rhs<N; rhs++) {
+            edges.push_back(Edge(lhs, rhs, get_min_len(loc[lhs], loc[rhs])));
+        }
+    }
+    
+    sort(edges.begin(), edges.end());
+
+    int sum = 0;
+    for(int i=0; i<edges.size(); i++) {
+
+        if(!Edge::is_sibling(edges[i].node[0], edges[i].node[1])) {
+            
+            sum += edges[i].distance;
+            Edge::union_parent(edges[i].node[0], edges[i].node[1]);
+        }
+    }
+
+    cout << sum << "\n";
+
+    return 0;
+}
