4c2c4a9f8124091d2104a5223848ea9c317a6602
dw.seo <will6678@naver.com>
2021-11-27 11:48:51 +0900

    change code for 14567

--- 동욱/211128/14567/main.cpp
+++ 동욱/211128/14567/main.cpp
@@ -11,61 +11,41 @@ int main() {
     int N, M;
     cin >> N >> M;
 
-    vector<vector<int>> subs_vec(N+1);
+    vector<vector<int>> subs_vector(N+1);
+    vector<int> in_degree(N+1);
+    queue<int> sub_queue;
 
 
+    int lhs, rhs;
     for(int i=0; i<M; i++) {
 
-        int lhs, rhs;
         cin >> lhs >> rhs;
 
-        subs_vec[lhs].push_back(rhs);
+        subs_vector[lhs].push_back(rhs);
+        in_degree[rhs]++;
     }
 
-    vector<int> in_degree(N+1);
-
-    for(int i=0; i<subs_vec.size(); i++) {
-
-        for(auto sub : subs_vec[i]) {
-            in_degree[sub]++;
-        }
-    }
-
-    queue<int> sub_queue;
-    int answer[N+1] = {0, };
-
-    // for(int i=1; i<N+1; i++) {
-    //     answer[i] = 1;
-    // }
-
-    int count = 1;
-    bool need_to_up = false;
+    vector<int> answer(N+1);
 
     for(int i=0; i<N; i++) {
-        
-        if(need_to_up) {
-            count++;
-            need_to_up = false;
-        }
-
         for(int index=1; index<=N; index++) {
             if(in_degree[index] == 0) {
                 sub_queue.push(index);
                 in_degree[index] = -1;
                 
-                // count++;
-                answer[index] = count;
-                // answer[index] = i+1;
-
-                need_to_up = true;
+                answer[index] = i+1;
             }
         }
 
-        auto res = sub_queue.front();
-        sub_queue.pop();
+        while(! sub_queue.empty()) {
 
-        for(auto sub : subs_vec[res]) {
-            in_degree[sub]--;
+            auto res = sub_queue.front();
+            sub_queue.pop();
+
+            for(auto sub : subs_vector[res]) {
+                if(in_degree[sub] > 0)
+                    in_degree[sub]--;
+            }
         }
 
     }
